a60d830c6fb1171dae3b299e2ae42f9705fbe602
f-rahim <freelancerrahim26@gmail.com>
2023-07-29 16:50:28 +0600

    Added about section

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <section className="hero-section py-10 md:py-20">
+      <section className="hero-area py-10 md:py-20">
         <div className="container">
           <div className="grid grid-cols-1 md:grid-cols-2 items-center">
             <div className="hero-text">
@@ -19,6 +19,41 @@ export default function Home() {
               </div>
             </div>
             <div className="hero-image">
+              <Image
+                src={"/hero.png"}
+                alt="Hero image"
+                width={1000}
+                height={100}
+                className="mt-12 md:mt-0"
+              ></Image>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="about-area py-10 md:py-20 bg-gray-50" id="about">
+        <div className="container">
+          <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-10">
+            <div className="about-text">
+              <h3 className="title">About me</h3>
+              <h4 className="font-semibold">
+                Frontend developer with React JS.
+              </h4>
+              <p className="mt-5 text-[--secondary-color]">
+                Hello! I am Abdur Rahim, a passionate Frontend developer. I
+                develop web applications. My core skill is based on JavaScript
+                and I love to do most of the things using JavaScript. I love to
+                make the web more open to the world. I have graduated with a
+                bachelor degree in Electrical Engineering from Jessore
+                Polytechnic Institure at Jessore, Bangladesh in 2021. I am
+                available for any kind of job that suits skills.
+              </p>
+              <div className="hero-action mt-10">
+                <Link href="#">Get Resume</Link>
+                <Link href="#">About Me</Link>
+              </div>
+            </div>
+            <div className="about-image">
               <Image
                 src={"/hero.png"}
                 alt="Hero image"
